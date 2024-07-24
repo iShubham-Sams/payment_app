7f55278f4bd2b91a797ed3871bff5f4bb3779fc2
bhandari_shubham <shubhambhandari12345hb@gmail.com>
2024-07-24 17:31:32 +0530

    redux add and react add for user front

--- apps/user-front/src/App.tsx
+++ apps/user-front/src/App.tsx
@@ -1,5 +1,22 @@
+import { useDispatch, useSelector } from "react-redux";
 import "./App.css";
+import { RootState } from "./redux";
+import { decrement, increment } from "./redux/sliceState/counterSlice";
 
 export default function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  const count = useSelector((state: RootState) => state.counter.value);
+  const dispatch = useDispatch();
+  return (
+    <div>
+      <div>
+        <button aria-label="Increment value" onClick={() => dispatch(increment())}>
+          Increment
+        </button>
+        <span>{count}</span>
+        <button aria-label="Decrement value" onClick={() => dispatch(decrement())}>
+          Decrement
+        </button>
+      </div>
+    </div>
+  );
 }
