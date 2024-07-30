ae0adb0cd076e1371850c030080c74437f6f2742
bhandari_shubham <shubhambhandari12345hb@gmail.com>
2024-07-30 16:39:10 +0530

    issue fixed

---
+++ packages/zodValidate/src/user/index.ts
@@ -0,0 +1,61 @@
+import z from "zod";
+
+export const registerUserZodSchema = z.object({
+    body: z.object({
+        name: z
+            .string({
+                required_error: "User name is required",
+                invalid_type_error: "User name must be a string",
+            })
+            .min(8),
+        email: z
+            .string({
+                required_error: "Email is required",
+                invalid_type_error: "Please send valid email",
+            })
+            .email(),
+        number: z.string({
+            required_error: "number required",
+        }),
+        password: z.string({
+            required_error: "Password required",
+        }),
+    }),
+});
+
+export const loginUserZodSchema = z.object({
+    body: z.object({
+        username: z
+            .string({
+                required_error: "Email or username required",
+                invalid_type_error: "User name must be a string",
+            })
+            .min(8)
+            .optional(),
+        email: z
+            .string({
+                required_error: "Email is required",
+                invalid_type_error: "Please send valid email",
+            })
+            .email()
+            .optional(),
+        password: z.string({
+            required_error: "Password required",
+        }),
+    }),
+});
+
+
+export const updateAccountDetailsZodSchema = z.object({
+    body: z.object({
+        name: z.string({
+            invalid_type_error: "Full name must be a string",
+        }).optional(),
+        email: z
+            .string({
+                invalid_type_error: "Please send valid email",
+            })
+            .email().optional(),
+        password: z.string().optional(),
+    }),
+});
